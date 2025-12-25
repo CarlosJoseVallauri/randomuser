a74f4bb02a6f7b9ab5c542e7b124f2af93a261c2
Carlo <carloriso2008@gmail.com>
2025-12-25 13:07:18 +0100

    Should be finally complete

--- js/dom.js
+++ js/dom.js
@@ -1,196 +1,201 @@
-let loadedArray = [];
-let searchArray;
-let currentPerson = 0;
+$(() => {
+    let loadedArray = [];
+    let searchArray;
+    let currentPerson = 0;
 
-listeners();
-datasets();
-firstload();
+    listeners();
+    datasets();
+    firstload();
 
-function firstload() {
-    load({results: 1}, loadPerson);
-}
+    async function firstload() {
+        currentPerson = 0;
+        loadedArray = await load({ results: 1 }, loadPerson);
+    }
 
-function listeners() {
-    $("#icons").on("mouseover", "li", function () {
-        $("#icons li.active").removeClass("active");
-        $(this).addClass("active");
-        $("#title").text($(this).data("title"));
-        $("#content").text($(this).data("content"));
-    });
+    function listeners() {
+        $("#icons").on("mouseover", "li", function () {
+            $("#icons li.active").removeClass("active");
+            $(this).addClass("active");
+            $("#title").text($(this).data("title"));
+            $("#content").text($(this).data("content"));
+        });
 
-    $("#rnNum").on("input", function () { $(this).siblings("label").text(`Numero persone: ${this.value}`) });
+        $("#rnNum").on("input", function () { $(this).siblings("label").text(`Numero persone: ${this.value}`) });
 
-    $(".bi-left").on("click", () => {
-        let person;
-        if(--currentPerson < 0){
-            if(searchArray){
-                currentPerson = searchArray.length - 1;
+        $(".bi-left").on("click", () => {
+            let person;
+            if (--currentPerson < 0) {
+                if (searchArray) {
+                    currentPerson = searchArray.length - 1;
+                }
+                else {
+                    currentPerson = loadedArray.length - 1;
+                }
             }
-            else{
-                currentPerson = loadedArray.length - 1;
-            }
-        }  
-        
-        person = searchArray ? searchArray.at(currentPerson) : loadedArray.at(currentPerson);
-        loadPerson(person);
-        loadJSON(person);
-    });
 
-    $(".bi-right").on("click", () => {
-        let person;
-        if(searchArray){
-            if(++currentPerson >= searchArray.length){
+            person = searchArray ? searchArray.at(currentPerson) : loadedArray.at(currentPerson);
+            loadPerson(person);
+            loadJSON(person);
+        });
+
+        $(".bi-right").on("click", () => {
+            let person;
+            if (searchArray) {
+                if (++currentPerson >= searchArray.length) {
+                    currentPerson = 0;
+                }
+                person = searchArray.at(currentPerson);
+            }
+            else {
+                if (++currentPerson >= loadedArray.length) {
+                    currentPerson = 0;
+                }
+                person = loadedArray.at(currentPerson);
+            }
+
+            loadPerson(person);
+            loadJSON(person);
+        });
+
+        $("#search").on("input", function () {
+            const VALUE = $(this).val();
+            const RESULT = loadedArray.deepSearch(VALUE);
+            $("i").addClass("invisible");
+
+            if (RESULT.at(0)) {
+                searchArray = RESULT;
+
+                if (RESULT.length !== 1) {
+                    $("i").removeClass("invisible");
+                }
+
                 currentPerson = 0;
+                loadPerson(RESULT.at(currentPerson));
             }
-            person = searchArray.at(currentPerson);
-        }
-        else{
-            if(++currentPerson >= loadedArray.length){
-                currentPerson = 0;
+            else {
+                searchArray = undefined;
+                loadEmpty();
             }
-            person = loadedArray.at(currentPerson);
-        }
+        });
 
-        loadPerson(person);
-        loadJSON(person);
-    });
+        $("#btnReset").on("click", () => {
+            $("#genAll").prop("checked", true);
+            $("input[name=chkNat]").prop("checked", false);
+            $("#rnNum").val(1).siblings("label").text(`Numero persone: 1`);
+        });
 
-    $("#search").on("input", function(){
-        const VALUE = $(this).val();
-        const RESULT = loadedArray.deepSearch(VALUE);
-        $("i").addClass("invisible");
+        $("#btnRequest").on("click", async () => {
+            searchArray = undefined;
 
-        if(RESULT.at(0)){
-            searchArray = RESULT;
-            
-            if(RESULT.length !== 1){
+            const PARAMS = {
+                results: parseInt($("#rnNum").val()),
+                gender: $("input[name=rdGen]:checked").val(),
+                nat: $("input[name=chkNat]:checked").map((_, ck) => ck.value).toArray().join(",")
+            };
+
+            loadedArray = await load(PARAMS, loadPerson);
+            currentPerson = 0;
+
+            if (PARAMS.results === 1) {
+                $("i").addClass("invisible");
+            }
+            else {
                 $("i").removeClass("invisible");
             }
+        });
 
-            loadPerson(RESULT.at(0));
-        }
-        else{
-            searchArray = undefined;
-            loadEmpty();
-        }
-    });
+        $("#sortList li").on("click", function () {
+            loadedArray.sort(SORTFUNCS[$(this).data("sort")]);
+            loadPerson(loadedArray[currentPerson = 0]);
+        });
 
-    $("#btnReset").on("click", () => {
-        $("#genAll").prop("checked", true);
-        $("input[name=chkNat]").prop("checked", false);
-        $("#rnNum").val(1).siblings("label").text(`Numero persone: 1`);
-    });
+        $("#btnShow").on("click", function () {
+            if ($(this).text() === "Nascondi JSON") {
+                $("andypf-json-viewer").remove();
+                $(this).text("Mostra JSON");
+                return;
+            }
 
-    $("#btnRequest").on("click", async () => {
-        searchArray = undefined;
+            $("<andypf-json-viewer/>")
+                .attr({
+                    "data": JSON.stringify(loadedArray[currentPerson]),
+                    "theme": "ia-dark",
+                    "indent": "3",
+                    "expanded": "true",
+                    "show-copy": "false"
+                })
+                .appendTo("#viewer");
 
-        const PARAMS = {
-            results: parseInt($("#rnNum").val()),
-            gender: $("input[name=rdGen]:checked").val(),
-            nat: $("input[name=chkNat]:checked").map((_, ck) => ck.value).toArray().join(",")
-        };
-    
-        await load(PARAMS, loadPerson);
+            $(this).text("Nascondi JSON");
+        });
 
-        if(PARAMS.results === 1){
-            $("i").addClass("invisible");
-        }
-        else{
-            $("i").removeClass("invisible");
-        }
-    });
+        $("#btnDown").on("click", () => {
+            const person = searchArray ? searchArray[currentPerson] : loadedArray[currentPerson];
 
-    $("#sortList li").on("click", function(){
-        loadedArray.sort(SORTFUNCS[$(this).data("sort")]);
-        loadPerson(loadedArray[currentPerson = 0]);
-    });
+            $("<a>")
+                .attr({
+                    href: URL.createObjectURL(new Blob([JSON.stringify(person, null, 2)], { type: 'application/json' })),
+                    download: `${person.name.first}_${person.name.last}.json`
+                })
+                .get(0)
+                .click();
+        });
 
-    $("#btnShow").on("click", function(){
-        if($(this).text() === "Nascondi JSON"){
-            $("andypf-json-viewer").remove();
-            $(this).text("Mostra JSON");
-            return;
-        }
-
-        $("<andypf-json-viewer/>")
-            .attr({
-                "data": JSON.stringify(loadedArray[currentPerson]),
-                "theme": "ia-dark",
-                "indent": "3",
-                "expanded": "true",
-                "show-copy": "false"
-            })
-            .appendTo("#viewer");
-
-        $(this).text("Nascondi JSON");
-    });
-
-    $("#btnDown").on("click", () => {
-        const person = searchArray ? searchArray[currentPerson] : loadedArray[currentPerson];
-
-        $("<a>")
-            .attr({
-                href: URL.createObjectURL(new Blob([JSON.stringify(person, null, 2)], {type: 'application/json'})),
-                download: `${person.name.first}_${person.name.last}.json`
-            })
-            .get(0)
-            .click();
-    });
-
-    $(window).on("resize", () => {
-        if(bootstrapDetectBreakpoint().index < 2){
-            $("#btnShow").attr("disabled", "true");
-        }
-        else{
-            $("#btnShow").removeAttr("disabled");
-        }
-    })
-}
-
-function loadJSON(person){
-    if($("andypf-json-viewer").length === 1){
-        $("andypf-json-viewer").prop("data", JSON.stringify(person));
+        $(window).on("resize", () => {
+            if (bootstrapDetectBreakpoint().index < 2) {
+                $("#btnShow").attr("disabled", "true");
+            }
+            else {
+                $("#btnShow").removeAttr("disabled");
+            }
+        })
     }
-}
 
-function datasets() {
-    // Title dataset
-    Object.entries({
-        name: "Ciao, il mio nome è",
-        mail: "La mia mail è",
-        birthday: "Il mio compleanno è il",
-        address: "Abito in",
-        phone: "Il mio numero di telefono è",
-        password: "La mia password è"
-    }).forEach(title => $(`#icons li[data-fn=${title[0]}]`).data("title", title[1]));
-}
+    function loadJSON(person) {
+        if ($("andypf-json-viewer").length === 1) {
+            $("andypf-json-viewer").prop("data", JSON.stringify(person));
+        }
+    }
 
-function loadPerson(person) {
-    Object.entries({
-        name: `${person?.name?.first} ${person?.name?.last}`,
-        mail: person?.email,
-        birthday: new Date(person?.dob?.date).toLocaleDateString(),
-        address: `${person?.location?.street?.name} ${person?.location?.street?.number}`,
-        phone: person?.cell,
-        password: person?.login?.password
-    }).forEach(content => $(`#icons li[data-fn=${content[0]}]`).data("content", content[1]));
+    function datasets() {
+        // Title dataset
+        Object.entries({
+            name: "Ciao, il mio nome è",
+            mail: "La mia mail è",
+            birthday: "Il mio compleanno è il",
+            address: "Abito in",
+            phone: "Il mio numero di telefono è",
+            password: "La mia password è"
+        }).forEach(title => $(`#icons li[data-fn=${title[0]}]`).data("title", title[1]));
+    }
 
-    $("#title").show();
-    $("#image").attr("src", person?.picture?.large);
-    $("#content").text($("#icons li.active").data("content"));
-}
+    function loadPerson(person) {
+        Object.entries({
+            name: `${person?.name?.first} ${person?.name?.last}`,
+            mail: person?.email,
+            birthday: new Date(person?.dob?.date).toLocaleDateString(),
+            address: `${person?.location?.street?.name} ${person?.location?.street?.number}`,
+            phone: person?.cell,
+            password: person?.login?.password
+        }).forEach(content => $(`#icons li[data-fn=${content[0]}]`).data("content", content[1]));
 
-function loadEmpty(){
-    Object.entries({
-        name: "Nessuna persona trovata",
-        mail: "Nessuna persona trovata",
-        birthday: "Nessuna persona trovata",
-        address: "Nessuna persona trovata",
-        phone: "Nessuna persona trovata",
-        password: "Nessuna persona trovata"
-    }).forEach(content => $(`#icons li[data-fn=${content[0]}]`).data("content", content[1]));
+        $("#title").show();
+        $("#image").attr("src", person?.picture?.large);
+        $("#content").text($("#icons li.active").data("content"));
+    }
 
-    $("#title").hide();
-    $("#image").attr("src", "./img/default.png");
-    $("#content").text($("#icons li.active").data("content"));
-}
+    function loadEmpty() {
+        Object.entries({
+            name: "Nessuna persona trovata",
+            mail: "Nessuna persona trovata",
+            birthday: "Nessuna persona trovata",
+            address: "Nessuna persona trovata",
+            phone: "Nessuna persona trovata",
+            password: "Nessuna persona trovata"
+        }).forEach(content => $(`#icons li[data-fn=${content[0]}]`).data("content", content[1]));
+
+        $("#title").hide();
+        $("#image").attr("src", "./img/default.png");
+        $("#content").text($("#icons li.active").data("content"));
+    }
+});
--- js/load.js
+++ js/load.js
@@ -2,6 +2,6 @@ async function load(params, callback){
     const response = await ajax.sendRequest("GET", "/api", params).catch(ajax.errore);
     const results = response?.data?.results;
 
-    loadedArray = results;
     callback(results.at(0));
+    return results;
 }
